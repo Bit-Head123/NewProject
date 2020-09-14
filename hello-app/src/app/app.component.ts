5929c6ce2b4933488cb52de83069f7c40416bc4e
Bit-Head123 <mitshu1221@gmail.com>
2020-09-14 16:01:24 +0530

    "Hello Tushar's commit

--- hello-app/src/app/app.component.ts
+++ hello-app/src/app/app.component.ts
@@ -11,6 +11,7 @@ import { Component } from '@angular/core';
       <h2>Github Jenkins integration attempt bby Abhinav</h2>
       <h2>Github Jenkins integration attempted by Mitshu </h2>
       <h2> Github Jenkins integreation attemted by Arnnab</h2>
+      <h2>Tushar's commit in hello app</h2>
       </div>
     
   `,
